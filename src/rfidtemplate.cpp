33905d462ee93b0040f27d594cb81dd575fcb061
adojang <21786275@sun.ac.za>
2023-05-06 14:06:44 +0200

    RFID Sensor

---
+++ src/rfidtemplate.cpp
@@ -0,0 +1,255 @@
+/*
+  --------------------------------------------------------------------------
+  Escape Room Template
+  Adriaan van Wijk
+  22 May 2023
+
+  RFID Template To Use
+
+  Copyright [2023] [Proxonics (Pty) Ltd]
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  --------------------------------------------------------------------------
+*/
+
+#define NAME "RFIDtemplate"
+#define MACAD 0xEE // Refer to Table in Conventions
+
+#define SS_PIN  5  // ESP32 pin GIOP5 
+#define RST_PIN 27 // ESP32 pin GIOP27
+/* Data Naming Convention for Mac Addresses
+
+*  0x00 - masterserver
+
+
+ * 0xA0 - humanchain
+ * 0xA1 - bikelight
+ * 0xA2 - clockmotor
+
+ * 0xB0 - beetle
+ * 0xB1 - chalicessensor
+ * 0xB2 - ringreader
+ * 0xB3 - tangrumtomb
+
+ * 0xC0 - thumbreader
+ * 0xC1 - Keypad 1
+ * 0xC2 - Keypad 2
+
+ * 0xD0 - relaycontrol
+
+ * 0xEE - template
+
+ * 0xF0 - Reserved for RFID
+
+
+
+*/
+
+
+
+
+/* Kernal*/
+#include <Arduino.h>
+#include <config.h>
+
+/* ESP-DASH */
+#include <ArduinoJson.h>
+#include <AsyncTimer.h>
+
+/* Wifi and mDNS */
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <ESPmDNS.h>
+
+/* ESP-NOW */
+#include <esp_now.h>
+
+/* Elegant OTA */
+#include <AsyncElegantOTA.h>
+
+/* RFID */
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress[] = {0x32, 0xAE, 0xA4, 0x07, 0x0D, 0x00}; // Address of Master Server
+uint8_t setMACAddress[] = {0x32, 0xAE, 0xA4, 0x07, 0x0D, MACAD};
+
+
+/* ESP Async Timer */
+AsyncTimer asynctimer;
+
+/* WiFi Credentials */
+const char* ssid = WIFI_SSID; // SSID
+const char* password = WIFI_PASS; // Password
+
+/* ESP-NOW Structures */
+typedef struct dataPacket {
+int trigger = 0;
+} dataPacket;
+
+dataPacket sData; // data to send
+dataPacket rData; // data to recieve
+
+/* Setup */
+AsyncWebServer server(80);
+esp_now_peer_info_t peerInfo;
+
+
+bool turnlighton = false;
+
+
+/* ESP-NOW Callback Functions*/
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Packet Delivery Success" : "Packet Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&rData, incomingData, sizeof(rData));
+  Serial.println("Override Data Recieved...");
+
+  if (rData.trigger == 1) {
+  //You should never use delay in this function. It might cause the ESP-NOW to crash.
+    turnlighton = true;
+  }
+  else
+  {
+    turnlighton = false;
+  }
+  
+  // Add your code here to do something with the data recieved.
+  //It's probably best to use a flag instead of calling it directly here. Not Sure
+
+
+}
+ 
+
+void startwifi(){
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_mac(WIFI_IF_STA, &setMACAddress[0]);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.printf("WiFi Failed!\n");
+      return;
+  }
+
+   /* MDNS */
+  if (!MDNS.begin(NAME)) {
+        Serial.println("Error setting up MDNS responder!");
+        while(1) {
+            delay(1000);
+        }
+    }
+  Serial.println("mDNS responder started");
+  Serial.printf("*** PROGRAM START ***\n\n");
+  
+  AsyncElegantOTA.begin(&server, "admin", "admin1234");
+
+  server.begin();
+  MDNS.addService("http", "tcp", 80);
+
+}
+
+void startespnow(){
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  //Register Callback Functions
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+}
+
+void setup() {
+  Serial.begin(115200);
+  startwifi();
+  startespnow();
+
+  //Make any Edits you need to add below this line ------------------------------
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2,LOW);
+
+
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
+
+
+
+}
+
+
+void loop() {
+
+  //This line is sort of required. It automatically sends the data every 5 seconds. Don't know why. But hey there it is.
+  asynctimer.setInterval([]() {esp_now_send(broadcastAddress, (uint8_t *) &sData, sizeof(sData));},  5000);
+
+  if (turnlighton) {
+    digitalWrite(2,HIGH);
+  }
+  else {
+    digitalWrite(2,LOW);
+  }
+
+
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
+
+
+
+  //Required for the asynctimer to work.
+  asynctimer.handle();
+}
