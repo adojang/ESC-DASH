078603512081a1e7179b9192d09de785b605e703
Gideon11 <35260235+Gideon11@users.noreply.github.com>
2023-05-17 06:34:38 +0200

    tangrimtomb.cpp edited

--- src/tangrumtomb.cpp
+++ src/tangrumtomb.cpp
@@ -75,6 +75,38 @@ int trigger = 0;
 dataPacket sData; // data to send
 dataPacket rData; // data to recieve
 
+/*Pin Variables*/
+int Onboard_LED = 2;  //Onboard blue LED pin 
+int ReedSwitchPin_1 = 4; //Reed switch output 1
+int ReedSwitchPin_2 = 13; //Reed switch output 2
+int ReedSwitchPin_3 = 14; //Reed switch output 3
+int ReedSwitchPin_4 = 16; //Reed switch output 4
+int ReedSwitchPin_5 = 17; //Reed switch output 5
+int ReedSwitchPin_6 = 18; //Reed switch output 6
+int ReedSwitchPin_7 = 19; //Reed switch output 7
+int ReedSwitchPin_8 = 21; //Reed switch output 8
+int ReedSwitchPin_9 = 22; //Reed switch output 9
+int ReedSwitchPin_10 = 23; //Reed switch output 10
+int ReedSwitchPin_11 = 25; //Reed switch output 11
+int ReedSwitchPin_12= 26; //Reed switch output 12
+int ReedSwitchPin_13 = 27; //Reed switch output 13
+int ReedSwitchPin_14 = 32; //Reed switch output 14
+int ReedSwitchPin_15 = 33; //Reed switch output 15
+
+int MagneticLockRelayPin = 16;  //Maglock relay activation pin 
+
+/*Other Global Variables*/
+int PuzzleSolved = 0;
+//int PuzzlesolvedPrev = 0;
+//int MaglockActivationTrigger = 0;
+//int MaglockTimerStart = 0;
+//int CurrentMillis = 0;
+//int OverrideTrigger = 0;
+int ledState = LOW;
+unsigned long PreviousMillis = 0;
+const long BlinkDelayInterval = 30;
+unsigned long CurrentMillis = 0;
+
 /* Setup */
 AsyncWebServer server(80);
 esp_now_peer_info_t peerInfo;
@@ -154,24 +186,88 @@ void startup(){
 
 }
 
+void Blink(){ //Function that blinks the blue LED onboard the ESP32
+if (CurrentMillis - PreviousMillis >= BlinkDelayInterval) {
+    // save the last time you blinked the LED
+    PreviousMillis = CurrentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(Onboard_LED, ledState);
+  }
+}
+
+int IsPuzzleSolved(){ //Function that reads all the reed switch inputs and determines if puzzle is solved or not
+  if(digitalRead(ReedSwitchPin_1)==1){ Serial.println("Reed1: ON");
+    if(digitalRead(ReedSwitchPin_2)==1){Serial.println("Reed2: ON");
+      if(digitalRead(ReedSwitchPin_3)==1){Serial.println("Reed3: ON");
+        if(digitalRead(ReedSwitchPin_4)==1){Serial.println("Reed4: ON");         
+          if(digitalRead(ReedSwitchPin_5)==1){Serial.println("Reed5: ON");
+            if(digitalRead(ReedSwitchPin_6)==1){Serial.println("Reed6: ON");
+              if(digitalRead(ReedSwitchPin_7)==1){Serial.println("Reed7: ON");
+                if(digitalRead(ReedSwitchPin_8)==1){Serial.println("Reed8: ON");
+                  if(digitalRead(ReedSwitchPin_9)==1){Serial.println("Reed9: ON");
+                    if(digitalRead(ReedSwitchPin_10)==1){Serial.println("Reed10: ON");
+                      if(digitalRead(ReedSwitchPin_11)==1){Serial.println("Reed11: ON");
+                        if(digitalRead(ReedSwitchPin_12)==1){Serial.println("Reed12: ON");
+                          if(digitalRead(ReedSwitchPin_13)==1){Serial.println("Reed13: ON");
+                            if(digitalRead(ReedSwitchPin_14)==1){Serial.println("Reed14: ON");
+                              if(digitalRead(ReedSwitchPin_15)==1){Serial.println("Reed15: ON");
+                                Blink();         
+                                return 1;
+                              }else{return 0;}
+                            }else{return 0;}
+                          }else{return 0;}
+                        }else{return 0;}
+                      }else{return 0;}
+                    }else{return 0;}
+                  }else{return 0;}
+                }else{return 0;}              
+              }else{return 0;}
+            }else{return 0;}
+          }else{return 0;}
+        }else{return 0;}
+      }else{return 0;}
+    }else{return 0;}
+  }else{return 0;}  
+}
 
 void setup() {
 
   Serial.begin(115200);
   startup(); // Startup for Wifi, mDNS, and OTA
   startespnow(); // Startup for ESP-NOW
-  //pinMode(, INPUT);
-
-
+  pinMode(Onboard_LED,OUTPUT); //Onboard LED pin configuration
+  pinMode(ReedSwitchPin_1, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_2, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_3, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_4, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_5, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_6, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_7, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_8, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_9, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_10, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_11, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_12, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_13, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_14, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
+  pinMode(ReedSwitchPin_15, INPUT_PULLDOWN);  //Use Pull-Down resistor configuration with GPIO pin as digital input
   //Begin Sending Data to Remote ESP's every 250ms
-  // asynctimer.setInterval([]() { esp_now_send(broadcastAddress, (uint8_t *) &sData, sizeof(sData));},  250);
+   asynctimer.setInterval([]() { esp_now_send(broadcastAddress, (uint8_t *) &sData, sizeof(sData));},  250);
 
 }
 
 
 void loop() {
   //Insert Code Here
-
+  PuzzleSolved=IsPuzzleSolved();
 
 
   //You need to edit OnDataRecv to handle incoming overrides.
