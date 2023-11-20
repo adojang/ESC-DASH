618aef5ce8c551401ed8deaf3a9a487ef20b2a5c
adojang <21786275@sun.ac.za>
2023-11-20 12:51:32 +0200

    Fix problems

--- src/mosfetA.cpp
+++ src/mosfetA.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include <EscCore.h>
+
+
+#define NAME "RFID4"
+#define setMACAddress m_RFID4
+AsyncTimer asynctimer(35);
+AsyncWebServer server(80);
+ESPDash dashboard(&server,false);
+esp_now_peer_info_t peerInfo;
+EscCore Core;
+
+uint8_t m_RFID4[] = {0x32, 0xAE, 0xA4, 0x07, 0x0D, 0xA7};
+
+int livecount = 0;
 
 void setup(){
-Serial.begin(15200);
-pinMode(16,OUTPUT);
+Serial.begin(115200);
+pinMode(4,OUTPUT);
+pinMode(2,OUTPUT);
 
-pinMode(25,OUTPUT);
-digitalWrite(25,HIGH);
+  Core.startup(setMACAddress, NAME, server);
+
+}
+
+void trigger(){
+    digitalWrite(4,HIGH);
+    digitalWrite(2,HIGH);
+    delay(100);
+    digitalWrite(4,LOW);
+    digitalWrite(2,LOW);
+    livecount++;
+
 
 }
 
 void loop(){
-digitalWrite(16,HIGH);
-delay(3000);
-digitalWrite(16,LOW);
+    trigger();
+
+delay(random(0,10000));
+WebSerial.printf("Number of times Triggered: %d", livecount);
+
 delay(3000);
 
 }
