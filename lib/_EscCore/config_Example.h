e088865c13f7eb64d7702a5261d0b0e5c9e186b8
Adriaan <adriaan@proxonics.co.za>
2024-01-23 17:47:30 +0200

    update config_example docs

--- lib/_EscCore/config_Example.h
+++ lib/_EscCore/config_Example.h
@@ -1,2 +1,5 @@
+//rename this file 'config.h' and replace the ssid and wifi with the right ones.
+//remember to COPY this file, so the original will stay on github.
+
 #define WIFI_SSID "Ewfwfwfw"
 #define WIFI_PASS "wfwfwfw"
